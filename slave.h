ef10e7667b52e271f518fc6ce286ecf3e88b6f10
Kat_ <gatti8758@gmail.com>
2025-07-22 12:24:44 +0200

    aaa

--- slave.h
+++ slave.h
@@ -8,17 +8,47 @@ BoardState get ( )
 
 void correct ( )
 {
+	bool done = false;
+	
+	while (!done){
+		if(ClockPin == LOW && DataPin == LOW){
+			pinMode(DataPin, OUTPUT);
+			digitalWrite(DataPin, HIGH);
+			delay(10);
+			digitalWrite(DataPin, LOW);
+			pinMode(DataPin, INPUT);
+			done = true;
+		}
+	}
 	// wait for the stream to be 0 0 then
 	// send 0 1 to master
 }
 
 void error ( )
 {
+	bool done = false;
+	
+	while (!done){
+		if(ClockPin == LOW && DataPin == LOW){
+			pinMode(ClockPin, OUTPUT);
+			digitalWrite(ClockPin, HIGH);
+			delay(10);
+			digitalWrite(ClockPin, LOW);
+			pinMode(ClockPin, INPUT);
+			done = true;
+		}
+	}
 	// wait for the stream to be 0 0 then
 	// send 1 0 to master
 }
 
 bool is_end ( )
 {
+	bool ans = false;
+	if(ClockPin == HIGH && DataPin == HIGH){
+		ans = true;
+	}
+
+	return ans;
 	// checks for 1 1
 }
