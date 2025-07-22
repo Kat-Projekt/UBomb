3e6ee9275352070f013dbeeab86a2f7c8d443100
Kat_ <gatti8758@gmail.com>
2025-07-22 12:48:25 +0200

    a

---
+++ Modules/morse.h
@@ -0,0 +1,19 @@
+#include "../common.h"
+
+void setup ( )
+{
+	auto State = get ( );
+	// set up local variables that use the State
+	timeleft ( ); // inizialize timer for the modues that use it
+}
+
+void loop ( )
+{
+	// do the module things
+
+	// check fo 1 1
+	if ( is_end ( ) )
+	{
+		// do something?
+	}
+}
